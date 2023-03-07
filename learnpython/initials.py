d350dc831cb80b5144bc0cf45a8298567352e564
AmanS <aman.sinha@cgu.edu>
2023-03-07 09:53:45 -0800

    Create initials.py

---
+++ learnpython/initials.py
@@ -0,0 +1,21 @@
+# name Aman Sinha
+#fun fact I love soccer
+first_initial = """     
+                           A
+                          A  A
+                         A    A
+                        A      A
+                       AAAAAAAAAA
+                      A          A
+                     A            A """
+print(first_initial)
+
+second_initial = """  
+                           SSS
+                          S    S
+                          S
+                           SSS
+                               S
+                          S    S
+                           SSSS  """
+print(second_initial)
